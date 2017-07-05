f5e0363b0401c56e2ce44054fdffb983f8d19a30
Shea Matthew Kennisher <deidrelarroque@Deidres-MacBook-Air.local>
2017-07-05 10:50:55 -0500

    Add authenticate and make users sign in to see tasks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,7 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def authenticate!
+    redirect_to :signin unless current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get '/auth/:provider/callback' => 'sessions#create'
   get '/signout' => 'sessions#destroy', as: :signout
+  get '/signin' => 'sessions#new', as: :signin
 
   resources :user_tasks
 
